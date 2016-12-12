2938711341599992dfba8b00fa4fda58890fc711
imironica <ionut.mironica@gmail.com>
2016-12-12 09:56:01 +0200

    Add shape descriptors

---
+++ TACAI_Demo/Shape recognition/compute_shape_features.py
@@ -0,0 +1,72 @@
+#Operating system libraries
+import os, sys
+#numpy
+import numpy as np
+
+#Image libraries
+from skimage.feature import hog
+from scipy.misc import imread, imsave, imresize
+from skimage import color
+ 
+import pandas as pd
+import mahotas
+import pylab
+import cv2
+import csv
+#Don't show warnings (regarding the deprecate functions)
+import warnings
+warnings.filterwarnings("ignore")
+
+#Read image names from the root folder
+root = os.path.dirname(os.path.realpath(__file__)) + '\\ShapeDB\\';
+dirs = os.listdir(root);
+
+featuresList = ['HU','ZERNIKE','MOMENTS'];
+file_to_save = 'image_list.csv';
+
+images = [];
+labels = [];
+descriptors = [];
+
+#Take all the elements from the image directory and put them into a list
+for file in dirs:
+    images.append(file);
+
+#Save the list of the files from the directory
+df = pd.DataFrame(images, columns=["colummn"])
+df.to_csv(file_to_save, index=False)
+
+#Generate features
+print ('Generate features');
+
+for currentFeature in featuresList:
+    print (currentFeature);
+    print("....");
+
+    #initiate the list of the descriptors
+    descriptors = [];
+
+    #for each image from the directory
+    for file in images:
+
+        img = imread(root + file);
+        print (file);
+
+        if(currentFeature == 'ZERNIKE'):
+            img = color.rgb2gray(img);
+            hist = mahotas.features.zernike_moments(img, 21)
+            descriptors.append(hist);
+
+        if(currentFeature == 'HU'):
+            img = color.rgb2gray(img);
+            hist = cv2.HuMoments(cv2.moments(img)).flatten()
+            descriptors.append(hist);
+
+        if(currentFeature == 'MOMENTS'):
+            img = color.rgb2gray(img);
+            hist = cv2.moments(img)
+            descriptors.append(hist);
+
+    feature_filename = currentFeature + '.csv';
+    df = pd.DataFrame(descriptors)
+    df.to_csv(feature_filename, index=False)
